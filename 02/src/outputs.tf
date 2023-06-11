b4c19d8e4d643706f770eb6b7af28165424bd1c7
Andrey Drutskoy <drone.64@gmail.com>
2023-06-12 01:24:02 +0300

    add output

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,3 +1,9 @@
-output external_ip {
-    value = { instance_name = "platform-web" }
+output "platform-web_ip_address" {
+value = yandex_compute_instance.platform-web.network_interface.0.nat_ip_address
+description = "vm external ip"
+}
+
+output "platform-db_ip_address" {
+value = yandex_compute_instance.platform-db.network_interface.0.nat_ip_address
+description = "vm external ip"
 }
